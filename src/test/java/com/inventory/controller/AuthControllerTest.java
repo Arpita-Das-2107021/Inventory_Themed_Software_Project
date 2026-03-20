78a15e54e6537184b45918b7c2134954a648b830
Arpita-2107021 <das2107021@stud.kuet.ac.bd>
2026-03-20 15:58:49 +0600

    test(auth): fix @SpringBootTest configuration in AuthControllerTest

--- src/test/java/com/inventory/controller/AuthControllerTest.java
+++ src/test/java/com/inventory/controller/AuthControllerTest.java
@@ -15,7 +15,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@SpringBootTest(classes = com.inventory.InventoryApplication.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class AuthControllerTest {
